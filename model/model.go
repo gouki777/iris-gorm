4bf54b5071cd77ea8ecaed6ce04382b96af9f50e
gouki777 <yf.sun@qq.com>
2020-07-22 14:27:20 +0800

    update mysql struct

--- model/model.go
+++ model/model.go
@@ -4,8 +4,8 @@ import "time"
 
 // Movie db struct
 type Movie struct {
-	Id         int    `gorm:"column:Id;AUTO_INCREMENT;PRIMARY_KEY;"` //表字段名为：id
-	Name       string `gorm:"not null;column:Name;"`                 //表字段名为：name
+	Id         int    `gorm:"column:Id;AUTO_INCREMENT;PRIMARY_KEY;"` //table struct：id
+	Name       string `gorm:"not null;column:Name;"`                 //table struct：name
 	Download   string `gorm:"column:Download;"`
 	CreateTime time.Time
 	Sha256     string `gorm:"not null;column Sha256;"`
